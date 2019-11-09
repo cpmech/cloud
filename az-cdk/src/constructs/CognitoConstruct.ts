dde072dc664718da83569741d241b78b92590830
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-09 17:12:57 +1000

    Implement Facebook and Google Idps in Cognito

--- az-cdk/src/constructs/CognitoConstruct.ts
+++ az-cdk/src/constructs/CognitoConstruct.ts
@@ -1,9 +1,12 @@
 import { Construct } from '@aws-cdk/core';
 import {
-  UserPool,
-  SignInType,
-  UserPoolAttribute,
   CfnUserPool,
+  CfnUserPoolClient,
+  CfnUserPoolDomain,
+  CfnUserPoolIdentityProvider,
+  SignInType,
+  UserPool,
+  UserPoolAttribute,
   UserPoolClient,
   UserPoolTriggers,
 } from '@aws-cdk/aws-cognito';
@@ -13,6 +16,14 @@ export interface ICognitoProps {
   emailSendingAccount: string;
   customAttributes?: string[]; // string, and mutable. NOTE: do not prefix with custom
   lambdaTriggers?: UserPoolTriggers;
+  customDomain?: string;
+  customDomainCertArn?: string;
+  facebookClientId?: string;
+  facebookClientSecret?: string;
+  googleClientId?: string;
+  googleClientSecret?: string;
+  callbackUrls?: string[];
+  logoutUrls?: string[];
 }
 
 export class CognitoConstruct extends Construct {
@@ -22,6 +33,7 @@ export class CognitoConstruct extends Construct {
   constructor(scope: Construct, id: string, props: ICognitoProps) {
     super(scope, id);
 
+    // pool
     const pool = new UserPool(this, 'Pool', {
       userPoolName: props.poolName,
       signInType: SignInType.EMAIL,
@@ -29,30 +41,115 @@ export class CognitoConstruct extends Construct {
       lambdaTriggers: props.lambdaTriggers,
     });
 
+    this.poolId = pool.userPoolId;
+
+    // client
     const client = new UserPoolClient(this, 'PoolCient', {
       userPoolClientName: `${props.poolName}-client`,
       userPool: pool,
+      generateSecret: false, // Amplify cannot handle secrets
     });
 
-    this.poolId = pool.userPoolId;
     this.clientId = client.userPoolClientId;
 
+    // constants
     const region = pool.stack.region;
     const accountId = pool.stack.account;
+    const cfnPool = pool.node.findChild('Resource') as CfnUserPool;
 
-    const cfn = pool.node.findChild('Resource') as CfnUserPool;
-
-    cfn.emailConfiguration = {
+    // set sending email
+    cfnPool.emailConfiguration = {
       emailSendingAccount: 'DEVELOPER',
       sourceArn: `arn:aws:ses:${region}:${accountId}:identity/${props.emailSendingAccount}`,
     };
 
+    // add custom attributes
     if (props.customAttributes) {
-      cfn.schema = props.customAttributes.map(name => ({
+      cfnPool.schema = props.customAttributes.map(name => ({
         attributeDataType: 'String',
         mutable: true,
         name,
       }));
+    }
+
+    // some flags
+    const hasCustomDomain = !!(props.customDomain && props.customDomainCertArn);
+    const hasFacebook = !!(props.facebookClientId && props.facebookClientSecret);
+    const hasGoogle = !!(props.googleClientId && props.googleClientSecret);
+    const hasIdp = hasFacebook || hasGoogle;
+
+    // custom domain
+    if (hasCustomDomain) {
+      new CfnUserPoolDomain(this, 'PoolDomain', {
+        userPoolId: pool.userPoolId,
+        domain: props.customDomain as string,
+        customDomainConfig: {
+          certificateArn: props.customDomainCertArn,
+        },
+      });
+    }
+
+    // Facebook identity provider
+    if (hasFacebook) {
+      new CfnUserPoolIdentityProvider(this, 'PoolFacebookIdP', {
+        userPoolId: pool.userPoolId,
+        providerName: 'Facebook',
+        providerType: 'Facebook',
+        providerDetails: {
+          client_id: props.facebookClientId,
+          client_secret: props.facebookClientSecret,
+          authorize_scopes: 'public_profile,email',
+        },
+        attributeMapping: {
+          email: 'email',
+          name: 'name',
+        },
+      });
+    }
+
+    // Google identity provider
+    if (hasGoogle) {
+      new CfnUserPoolIdentityProvider(this, 'PoolGoogleIdP', {
+        userPoolId: pool.userPoolId,
+        providerName: 'Google',
+        providerType: 'Google',
+        providerDetails: {
+          client_id: props.googleClientId,
+          client_secret: props.googleClientSecret,
+          authorize_scopes: 'profile email openid',
+        },
+        attributeMapping: {
+          email: 'email',
+          name: 'name',
+        },
+      });
+    }
+
+    // update pool client with IdPs data
+    if (hasIdp) {
+      const cfnClient = client.node.findChild('Resource') as CfnUserPoolClient;
+      cfnClient.allowedOAuthFlows = ['code'];
+      cfnClient.allowedOAuthFlowsUserPoolClient = true;
+      cfnClient.allowedOAuthScopes = [
+        'phone',
+        'email',
+        'openid',
+        'aws.cognito.signin.user.admin',
+        'profile',
+      ];
+      cfnClient.callbackUrLs = props.callbackUrls;
+      cfnClient.logoutUrLs = props.logoutUrls;
+      if (props.callbackUrls && props.callbackUrls.length > 0) {
+        cfnClient.defaultRedirectUri = props.callbackUrls[0];
+      }
+      cfnClient.supportedIdentityProviders = ['COGNITO'];
+      if (hasFacebook) {
+        cfnClient.supportedIdentityProviders.push('Facebook');
+      }
+      if (hasGoogle) {
+        cfnClient.supportedIdentityProviders.push('Google');
+      }
+      cfnClient.writeAttributes = ['email', 'name'];
     }
   }
 }
--- az-cdk/src/constructs/__tests__/CognitoConstruct.test.ts
+++ az-cdk/src/constructs/__tests__/CognitoConstruct.test.ts
@@ -13,3 +13,23 @@ describe('CognitoConstruct', () => {
     expect(synthAppString(app)).toMatchSnapshot();
   });
 });
+
+describe('CognitoConstruct + Identity Providers', () => {
+  it('synthetizes properly', () => {
+    const app = new App();
+    const stack = new Stack(app, 'Stack');
+    new CognitoConstruct(stack, 'Cognito', {
+      poolName: 'dear-users',
+      emailSendingAccount: 'tester@sender.com',
+      facebookClientId: '123412341',
+      facebookClientSecret: '12341asdas12341adf',
+      googleClientId: '12341324.apps.googleusercontent.com',
+      googleClientSecret: '123412341',
+      customDomain: 'https://myapp.custom.domain',
+      customDomainCertArn: 'arn:aws:acm:us-east-1:1234',
+      callbackUrls: ['https://localhost:3000/'],
+      logoutUrls: ['https://localhost:3000/'],
+    });
+    expect(synthAppString(app)).toMatchSnapshot();
+  });
+});
