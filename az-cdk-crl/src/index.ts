db6d85282c17ac44bf7464f26d155280285655fd
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-11 11:21:36 +1000

    Change how fcs are exported

--- az-cdk-crl/src/index.ts
+++ az-cdk-crl/src/index.ts
@@ -1,3 +1,3 @@
-export { route53Alias } from './route53Alias';
-export { sesDefaultRuleSet } from './sesDefaultRuleSet';
-export { verifyDomain } from './verifyDomain';
+export * from './route53Alias';
+export * from './sesDefaultRuleSet';
+export * from './verifyDomain';
