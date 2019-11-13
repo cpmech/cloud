2ec5f4f1ef9dbd21255aa3106fda59bfe3012203
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-13 19:18:00 +1000

    Update deps. Fix tests

--- az-cdk/src/constructs/__tests__/CognitoConstruct.test.ts
+++ az-cdk/src/constructs/__tests__/CognitoConstruct.test.ts
@@ -14,30 +14,6 @@ describe('CognitoConstruct', () => {
   });
 });
 
-describe('CognitoConstruct + users', () => {
-  it('synthetizes properly', () => {
-    const app = new App();
-    const stack = new Stack(app, 'Stack');
-    new CognitoConstruct(stack, 'Cognito', {
-      poolName: 'dear-users',
-      emailSendingAccount: 'tester@sender.com',
-      users: [
-        {
-          email: `tester@azcdk.xyz`,
-          password: '<pwd-goes-here>',
-          groups: 'testers',
-        },
-        {
-          email: `tester@azcdk.xyz`,
-          password: '<pwd-goes-here>',
-          groups: 'visitors,travellers',
-        },
-      ],
-    });
-    expect(synthAppString(app)).toMatchSnapshot();
-  });
-});
-
 describe('CognitoConstruct + Identity Providers', () => {
   it('synthetizes properly', () => {
     const app = new App();
@@ -49,11 +25,9 @@ describe('CognitoConstruct + Identity Providers', () => {
       facebookClientSecret: '12341asdas12341adf',
       googleClientId: '12341324.apps.googleusercontent.com',
       googleClientSecret: '123412341',
-      customDomain: 'https://myapp.custom.domain',
-      customDomainCertArn: 'arn:aws:acm:us-east-1:1234',
+      domainPrefix: 'myapp-login',
       callbackUrls: ['https://localhost:3000/'],
       logoutUrls: ['https://localhost:3000/'],
-      updateClientSettings: true,
     });
     expect(synthAppString(app)).toMatchSnapshot();
   });
