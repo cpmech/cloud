14a7ee6e8a51d6308f84c7ef3408a98be0b86336
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-01-16 17:50:42 +1000

    [az-cdk] Add options to Dynamo

--- az-cdk/src/constructs/DynamoConstruct.ts
+++ az-cdk/src/constructs/DynamoConstruct.ts
@@ -1,11 +1,25 @@
 import { Construct } from '@aws-cdk/core';
-import { Table, AttributeType, BillingMode } from '@aws-cdk/aws-dynamodb';
+import {
+  Table,
+  AttributeType,
+  BillingMode,
+  GlobalSecondaryIndexProps,
+  LocalSecondaryIndexProps,
+} from '@aws-cdk/aws-dynamodb';
 
 export interface IDynamoTable {
   name: string; // name of table
   partitionKey: string; // name of partiton key (the value will be string)
+  partitionKeyType?: AttributeType; // [default = STRING]
   sortKey?: string; // name of sort key (the value will be string)
+  sortKeyType?: AttributeType; // [default = STRING]
   onDemand?: boolean; // if false, will not touch default provisioned values
+  ttlAttribute?: string; // name of TTL attribute
+  pitRecovery?: boolean; // point in time recovery
+  readCapacity?: number; // read capacity for the table
+  writeCapacity?: number; // write capacity for the table
+  gsi?: GlobalSecondaryIndexProps;
+  lsi?: LocalSecondaryIndexProps;
 }
 
 export interface IDynamoApiProps {
@@ -19,12 +33,35 @@ export class DynamoConstruct extends Construct {
     super(scope, id);
 
     props.dynamoTables.forEach((t, i) => {
+      // new table
       const table = new Table(this, `Table${i}`, {
         tableName: t.name,
-        partitionKey: { name: t.partitionKey, type: AttributeType.STRING },
-        sortKey: t.sortKey ? { name: t.sortKey, type: AttributeType.STRING } : undefined,
+        partitionKey: {
+          name: t.partitionKey,
+          type: t.partitionKeyType ? t.partitionKeyType : AttributeType.STRING,
+        },
+        sortKey: t.sortKey
+          ? {
+              name: t.sortKey,
+              type: t.sortKeyType ? t.sortKeyType : AttributeType.STRING,
+            }
+          : undefined,
         billingMode: t.onDemand ? BillingMode.PAY_PER_REQUEST : undefined,
+        timeToLiveAttribute: t.ttlAttribute ? t.ttlAttribute : undefined,
+        pointInTimeRecovery: t.pitRecovery ? t.pitRecovery : undefined,
+        readCapacity: t.readCapacity ? t.readCapacity : undefined,
+        writeCapacity: t.writeCapacity ? t.writeCapacity : undefined,
       });
+
+      // indices
+      if (t.gsi) {
+        table.addGlobalSecondaryIndex(t.gsi);
+      }
+      if (t.lsi) {
+        table.addLocalSecondaryIndex(t.lsi);
+      }
+
+      // save
       this.name2table[t.name] = table;
     });
   }
