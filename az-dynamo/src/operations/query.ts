eaedded6af53c359b47b16c16f10afd41b8d464d
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-01-24 09:43:08 +1000

    [az-dynamo] rename rval

--- az-dynamo/src/operations/query.ts
+++ az-dynamo/src/operations/query.ts
@@ -27,11 +27,11 @@ export const query = async (
           [`#${rangeKeyName}`]: rangeKeyName,
         },
         ExpressionAttributeValues: {
-          ':hkey': hashKeyValue,
-          ':rkey': rangeKeyValue,
-          ':rkey2': rangeKeyValue2,
+          ':hval': hashKeyValue,
+          ':rval': rangeKeyValue,
+          ':rval2': rangeKeyValue2,
         },
-        KeyConditionExpression: `#${hashKeyName} = :hkey and #${rangeKeyName} BETWEEN :rkey AND :rkey2`,
+        KeyConditionExpression: `#${hashKeyName} = :hval and #${rangeKeyName} BETWEEN :rval AND :rval2`,
       })
       .promise();
     return data.Items ? data.Items : [];
@@ -48,13 +48,13 @@ export const query = async (
           [`#${rangeKeyName}`]: rangeKeyName,
         },
         ExpressionAttributeValues: {
-          ':hkey': hashKeyValue,
-          ':rkey': rangeKeyValue,
+          ':hval': hashKeyValue,
+          ':rval': rangeKeyValue,
         },
         KeyConditionExpression:
           op === 'prefix'
-            ? `#${hashKeyName} = :hkey and begins_with(#${rangeKeyName}, :rkey)`
-            : `#${hashKeyName} = :hkey and #${rangeKeyName} ${op} :rkey`,
+            ? `#${hashKeyName} = :hval and begins_with(#${rangeKeyName}, :rval)`
+            : `#${hashKeyName} = :hval and #${rangeKeyName} ${op} :rval`,
       })
       .promise();
     return data.Items ? data.Items : [];
@@ -66,8 +66,8 @@ export const query = async (
       TableName: table,
       IndexName: index,
       ExpressionAttributeNames: { [`#${hashKeyName}`]: hashKeyName },
-      ExpressionAttributeValues: { ':hkey': hashKeyValue },
-      KeyConditionExpression: `#${hashKeyName} = :hkey`,
+      ExpressionAttributeValues: { ':hval': hashKeyValue },
+      KeyConditionExpression: `#${hashKeyName} = :hval`,
     })
     .promise();
   return data.Items ? data.Items : [];
