400d29936d9354a5918b0b807d389a8419328280
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-01-23 18:57:58 +1000

    [az-dynamo] Implement use of Index

--- az-dynamo/src/operations/__tests__/query.test.ts
+++ az-dynamo/src/operations/__tests__/query.test.ts
@@ -9,41 +9,71 @@ AWS.config.update({
 });
 
 const tableName = 'TEST-AZDYN-USERS';
+const tableIndex = 'USERS-INDEX';
+
+const data1 = {
+  itemId: 'get',
+  aspect: 'ACCESS',
+  indexSK: '2020-01-19T01:02:01Z',
+  email: 'get@operation.com',
+  fullName: 'Tester Bot',
+  confirmed: true,
+  confirmMessageCount: 3,
+};
+
+const data2 = {
+  itemId: 'get',
+  aspect: 'LOCATION',
+  indexSK: '2020-01-19T01:02:02Z',
+  description: 'A Very Nice Place',
+  coordinates: { x: 1.1, y: 2.2, z: 3.3 },
+};
+
+const s1 = {
+  itemId: 'scan.1',
+  indexSK: '2020-01-01T01:02:08Z',
+  fullName: 'First Tester Bot',
+  email: 'scan.1@operation.com',
+  aspect: 'NAME',
+};
+const s2 = {
+  itemId: 'scan.2',
+  indexSK: '2020-01-02T01:02:09Z',
+  fullName: 'Second Tester Bot',
+  email: 'scan.2@operation.com',
+  aspect: 'NAME',
+};
+const s3 = {
+  itemId: 'scan.3',
+  indexSK: '2020-01-03T01:02:10Z',
+  fullName: 'Third Tester Bot',
+  email: 'scan.3@operation.com',
+  aspect: 'NAME',
+};
+const s4 = {
+  itemId: 'scan.4',
+  indexSK: '2020-01-04T01:02:11Z',
+  fullName: 'Fourth Tester Bot',
+  email: 'scan.4@operation.com',
+  aspect: 'NAME',
+};
+const s5 = {
+  itemId: 'scan.5',
+  indexSK: '2020-01-05T01:02:12Z',
+  fullName: 'Fifth Tester Bot',
+  email: 'scan.5@operation.com',
+  aspect: 'NAME',
+};
 
 describe('query operation', () => {
   it('should return all aspects', async () => {
     const res = await query(tableName, 'itemId', 'get');
-    expect(res).toEqual([
-      {
-        itemId: 'get',
-        aspect: 'ACCESS',
-        indexSK: '2020-01-19T01:02:01Z',
-        email: 'get@operation.com',
-        fullName: 'Tester Bot',
-        confirmed: true,
-        confirmMessageCount: 3,
-      },
-      {
-        itemId: 'get',
-        aspect: 'LOCATION',
-        indexSK: '2020-01-19T01:02:02Z',
-        description: 'A Very Nice Place',
-        coordinates: { x: 1.1, y: 2.2, z: 3.3 },
-      },
-    ]);
+    expect(res).toEqual([data1, data2]);
   });
 
   it('should return only LOCATION aspect', async () => {
     const res = await query(tableName, 'itemId', 'get', 'aspect', 'LOCATION');
-    expect(res).toEqual([
-      {
-        itemId: 'get',
-        aspect: 'LOCATION',
-        indexSK: '2020-01-19T01:02:02Z',
-        description: 'A Very Nice Place',
-        coordinates: { x: 1.1, y: 2.2, z: 3.3 },
-      },
-    ]);
+    expect(res).toEqual([data2]);
   });
 
   it('should return empty list on inexistent keys', async () => {
@@ -53,5 +83,44 @@ describe('query operation', () => {
     expect(r1).toEqual([]);
     expect(r2).toEqual([]);
     expect(r3).toEqual([]);
+  });
+
+  it('should query using "prefix"', async () => {
+    const r1 = await query(tableName, 'itemId', 'get', 'aspect', 'A', undefined, 'prefix');
+    const r2 = await query(tableName, 'itemId', 'get', 'aspect', 'L', undefined, 'prefix');
+    expect(r1).toEqual([data1]);
+    expect(r2).toEqual([data2]);
+  });
+
+  it('should query the index', async () => {
+    const res = await query(tableName, 'aspect', 'NAME', undefined, undefined, tableIndex);
+    expect(res).toEqual([s1, s2, s3, s4, s5]);
+  });
+
+  it('should query the index using "prefix"', async () => {
+    const res = await query(
+      tableName,
+      'aspect',
+      'NAME',
+      'indexSK',
+      '2020-01-01',
+      tableIndex,
+      'prefix',
+    );
+    expect(res).toEqual([s1]);
+  });
+
+  it('should query the index using "between"', async () => {
+    const res = await query(
+      tableName,
+      'aspect',
+      'NAME',
+      'indexSK',
+      '2020-01-01',
+      tableIndex,
+      'between',
+      '2020-01-03T12:00:00Z',
+    );
+    expect(res).toEqual([s1, s2, s3]);
   });
 });
--- az-dynamo/src/operations/query.ts
+++ az-dynamo/src/operations/query.ts
@@ -9,21 +9,65 @@ export const query = async (
   hashKeyValue: string,
   rangeKeyName?: string,
   rangeKeyValue?: string,
-  rangeKeyOp: '<' | '=' | '>' = '=',
+  index?: string, // index name
+  op: '<=' | '<' | '=' | '>' | '>=' | 'prefix' | 'between' = '=',
+  rangeKeyValue2?: string, // to be used with the 'between' op
 ): Promise<Iany[]> => {
+  // ddb object
   const ddb = new DynamoDB.DocumentClient();
+
+  // with hash and sort keys and a second value for the sort condition
+  if (rangeKeyName && rangeKeyValue2) {
+    const data = await ddb
+      .query({
+        TableName: table,
+        IndexName: index,
+        ExpressionAttributeNames: {
+          [`#${hashKeyName}`]: hashKeyName,
+          [`#${rangeKeyName}`]: rangeKeyName,
+        },
+        ExpressionAttributeValues: {
+          ':hkey': hashKeyValue,
+          ':rkey': rangeKeyValue,
+          ':rkey2': rangeKeyValue2,
+        },
+        KeyConditionExpression: `#${hashKeyName} = :hkey and #${rangeKeyName} BETWEEN :rkey AND :rkey2`,
+      })
+      .promise();
+    return data.Items ? data.Items : [];
+  }
+
+  // with hash and sort keys
+  if (rangeKeyName) {
+    const data = await ddb
+      .query({
+        TableName: table,
+        IndexName: index,
+        ExpressionAttributeNames: {
+          [`#${hashKeyName}`]: hashKeyName,
+          [`#${rangeKeyName}`]: rangeKeyName,
+        },
+        ExpressionAttributeValues: {
+          ':hkey': hashKeyValue,
+          ':rkey': rangeKeyValue,
+        },
+        KeyConditionExpression:
+          op === 'prefix'
+            ? `#${hashKeyName} = :hkey and begins_with(#${rangeKeyName}, :rkey)`
+            : `#${hashKeyName} = :hkey and #${rangeKeyName} ${op} :rkey`,
+      })
+      .promise();
+    return data.Items ? data.Items : [];
+  }
+
+  // just hash key
   const data = await ddb
     .query({
       TableName: table,
-      ExpressionAttributeNames: rangeKeyName
-        ? { [`#${hashKeyName}`]: hashKeyName, [`#${rangeKeyName}`]: rangeKeyName }
-        : { [`#${hashKeyName}`]: hashKeyName },
-      ExpressionAttributeValues: rangeKeyName
-        ? { ':hkey': hashKeyValue, ':rkey': rangeKeyValue }
-        : { ':hkey': hashKeyValue },
-      KeyConditionExpression: rangeKeyName
-        ? `#${hashKeyName} = :hkey and #${rangeKeyName} ${rangeKeyOp} :rkey`
-        : `#${hashKeyName} = :hkey`,
+      IndexName: index,
+      ExpressionAttributeNames: { [`#${hashKeyName}`]: hashKeyName },
+      ExpressionAttributeValues: { ':hkey': hashKeyValue },
+      KeyConditionExpression: `#${hashKeyName} = :hkey`,
     })
     .promise();
   return data.Items ? data.Items : [];
