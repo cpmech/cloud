959cbf9c4f96a018c333d11706f9483eae0b5010
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2020-03-17 10:19:48 -0300

    Feat: add attachments to email
    - add atachments to email
    - sender now accept a new format "Name LastName <name@email.com>"

--- az-senqs/src/sendEmail.ts
+++ az-senqs/src/sendEmail.ts
@@ -1,30 +1,27 @@
 import AWS from 'aws-sdk';
+import nodemailer from 'nodemailer';
+import { Attachment } from 'nodemailer/lib/mailer';
 
 export const sendEmail = async (
   sender: string,
   receivers: string[],
   subject: string,
   message: string,
+  attachments?: Attachment[],
   sqsConfig?: AWS.SQS.ClientConfiguration,
 ) => {
-  const params: AWS.SES.SendEmailRequest = {
-    Destination: {
-      ToAddresses: receivers,
-    },
-    Message: {
-      Subject: {
-        Charset: 'UTF-8',
-        Data: subject,
-      },
-      Body: {
-        Text: {
-          Charset: 'UTF-8',
-          Data: message,
-        },
-      },
-    },
-    Source: sender,
-  };
-  const ses = new AWS.SES(sqsConfig);
-  await ses.sendEmail(params).promise();
+
+  const transporter = nodemailer.createTransport({
+    SES: new AWS.SES(sqsConfig)
+  });
+
+  return await transporter.sendMail(
+    {
+      from: sender,
+      to: receivers,
+      subject,
+      text: message,
+      attachments: attachments!,
+    }
+  );
 };
