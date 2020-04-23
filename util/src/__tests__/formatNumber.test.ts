2115bc92a1dcb1f0f1bc2651cd5eb425b4a628f8
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-04-23 15:20:24 +1000

    [util] add tests

--- util/src/__tests__/formatNumber.test.ts
+++ util/src/__tests__/formatNumber.test.ts
@@ -38,17 +38,53 @@ describe('formatNumber', () => {
   });
 });
 
-describe('cleanNumber', () => {
-  it('should remove formatting', () => {
-    expect(cleanNumber('')).toBe('');
-    expect(cleanNumber('', true)).toBe('');
-    expect(cleanNumber('1,234')).toBe('1234');
-    expect(cleanNumber('1.234', true)).toBe('1234');
-    expect(cleanNumber('1,234.56')).toBe('1234.56');
-    expect(cleanNumber('1.234,56', true)).toBe('1234.56');
-    expect(cleanNumber('1,234.5678')).toBe('1234.5678');
-    expect(cleanNumber('1.234,5678', true)).toBe('1234.5678');
-    expect(cleanNumber('... 1,234.56', false, '...')).toBe('1234.56');
-    expect(cleanNumber('... 1.234,56', true, '...')).toBe('1234.56');
+describe('formatNumber (US)', () => {
+  it('should return the right value', () => {
+    expect(formatNumber('10.45.')).toBe('10.45');
+    expect(formatNumber('1.0.45')).toBe('1.04');
+    expect(formatNumber('1.0.45.')).toBe('1.04');
+    expect(formatNumber('.')).toBe('0.');
+    expect(formatNumber('00,000.1')).toBe('0.1');
+    expect(formatNumber('.45')).toBe('0.45');
+    expect(formatNumber('.45123')).toBe('0.45');
+    expect(formatNumber('1234')).toBe('1,234');
+    expect(formatNumber('12345678')).toBe('12,345,678');
+    expect(formatNumber('1234.123')).toBe('1,234.12');
+    expect(formatNumber('a1b2c3d4')).toBe('1,234');
+    expect(formatNumber('12,34')).toBe('1,234');
+    expect(formatNumber('$1234.456')).toBe('1,234.45');
+  });
+});
+
+describe('formatNumber (BR)', () => {
+  it('should return the right value', () => {
+    expect(formatNumber('10,45,', true)).toBe('10,45');
+    expect(formatNumber('1,0,45', true)).toBe('1,04');
+    expect(formatNumber('1,0,45.', true)).toBe('1,04');
+    expect(formatNumber(',', true)).toBe('0,');
+    expect(formatNumber('00.000,1', true)).toBe('0,1');
+    expect(formatNumber(',45', true)).toBe('0,45');
+    expect(formatNumber(',45123', true)).toBe('0,45');
+    expect(formatNumber('1234', true)).toBe('1.234');
+    expect(formatNumber('12345678', true)).toBe('12.345.678');
+    expect(formatNumber('1234,123', true)).toBe('1.234,12');
+    expect(formatNumber('a1b2c3d4', true)).toBe('1.234');
+    expect(formatNumber('12.34', true)).toBe('1.234');
+    expect(formatNumber('$1234,456', true)).toBe('1.234,45');
+  });
+
+  describe('cleanNumber', () => {
+    it('should remove formatting', () => {
+      expect(cleanNumber('')).toBe('');
+      expect(cleanNumber('', true)).toBe('');
+      expect(cleanNumber('1,234')).toBe('1234');
+      expect(cleanNumber('1.234', true)).toBe('1234');
+      expect(cleanNumber('1,234.56')).toBe('1234.56');
+      expect(cleanNumber('1.234,56', true)).toBe('1234.56');
+      expect(cleanNumber('1,234.5678')).toBe('1234.5678');
+      expect(cleanNumber('1.234,5678', true)).toBe('1234.5678');
+      expect(cleanNumber('... 1,234.56', false, '...')).toBe('1234.56');
+      expect(cleanNumber('... 1.234,56', true, '...')).toBe('1234.56');
+    });
   });
 });
