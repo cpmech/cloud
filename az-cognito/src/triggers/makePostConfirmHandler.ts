e12bdd7c2dfda9aabb922fb3c219377aa40ca025
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-25 18:29:50 +1000

    Improve make post confirm handler

--- az-cognito/src/triggers/makePostConfirmHandler.ts
+++ az-cognito/src/triggers/makePostConfirmHandler.ts
@@ -4,76 +4,89 @@ import { defaultEmailMaker, IEmailMaker } from './defaultEmailMaker';
 import { IEventCognito, ILambdaCognito } from './types';
 import { adminAddUserToGroup, adminSetAttributes } from '../admin';
 
-export type IUpdateDb = (userName: string, email: string, name?: string) => Promise<void>;
+export interface IPostConfirmUser {
+  userName: string;
+  email: string;
+  name?: string;
+}
+
+export type IUpdateDb = (user: IPostConfirmUser) => Promise<void>;
+
+export type ISelectGroup = (user: IPostConfirmUser) => string;
 
 export interface IPostConfirmInput {
-  updateDb?: IUpdateDb; // function to update database
-  defaultUserGroup?: string; // e.g. 'customers'
-  senderEmail?: string; // e.g. 'admin@example.com'
+  addToGroup?: ISelectGroup; // e.g. ({email}) => email == 'me@me.com' ? 'awesome' : 'customers'
   emailMaker?: IEmailMaker; // function to make emails
+  updateDb?: IUpdateDb; // function to update database
+  senderEmail?: string; // e.g. 'admin@example.com'
 }
 
 // makeCognitoPostConfirmHandler returns a lambda function to
 // act as a post-confirm handler for Cognito
-export const makeCognitoPostConfirmHandler = ({
-  defaultUserGroup,
-  senderEmail,
-  emailMaker,
-  updateDb,
-}: IPostConfirmInput): ILambdaCognito => async (event: IEventCognito): Promise<any> => {
-  // extract input
-  const { userName } = event;
-  const { email } = event.request.userAttributes;
-  const name = event.request.userAttributes.name || undefined;
-  mlog('... print(event) ...');
-  mlog(event);
+export const makeCognitoPostConfirmHandler =
+  ({ addToGroup, emailMaker, updateDb, senderEmail }: IPostConfirmInput): ILambdaCognito =>
+  async (event: IEventCognito): Promise<any> => {
+    // extract input
+    const { userName } = event;
+    const { email } = event.request.userAttributes;
+    const name = event.request.userAttributes.name || undefined;
+    mlog('... cognito post-confirm function ...');
+    mlog({ event });
 
-  // check userName and email ...');
-  mlog('... checking userName and email ...');
-  if (!userName) {
-    throw new Error(elog('cannot get userName from event data'));
-  }
-  if (!email) {
-    throw new Error(elog('cannot get email from event data'));
-  }
+    // check userName and email ...');
+    mlog('... checking userName and email ...');
+    if (!userName) {
+      throw new Error(elog('cannot get userName from event data'));
+    }
+    if (!email) {
+      throw new Error(elog('cannot get email from event data'));
+    }
 
-  // extract status and federation provider
-  mlog('... extracting status and federation provider ...');
-  const status = event.request.userAttributes['cognito:user_status'];
-  let provider;
-  if (status === 'EXTERNAL_PROVIDER') {
-    provider = userName.split('_')[0];
-    mlog(`... account created with ${provider} credentials ...`);
-  }
+    // extract status and federation provider
+    mlog('... extracting status and federation provider ...');
+    const status = event.request.userAttributes['cognito:user_status'];
+    let provider;
+    if (status === 'EXTERNAL_PROVIDER') {
+      provider = userName.split('_')[0];
+      mlog(`... account created with ${provider} credentials ...`);
+    }
 
-  // call updateDb function
-  if (updateDb) {
-    mlog('... calling updateDb function ...');
-    await updateDb(userName, email, name);
-  }
+    // set post-confirm user data
+    const user: IPostConfirmUser = { userName, email, name };
+    mlog({ user });
 
-  // add user to group
-  if (defaultUserGroup) {
-    mlog(`... adding user to group ${defaultUserGroup} ...`);
-    await adminAddUserToGroup(event.userPoolId, userName, defaultUserGroup);
-  }
+    // add user to group
+    if (addToGroup) {
+      const group = addToGroup(user);
+      if (!group) {
+        throw new Error(elog('addToGroup must return a non-empty string'));
+      }
+      mlog(`... adding user to group ${group} ...`);
+      await adminAddUserToGroup(event.userPoolId, userName, group);
+    }
 
-  // set email_verified attribute
-  if (status === 'EXTERNAL_PROVIDER') {
-    mlog('... setting email_verified attribute ...');
-    await adminSetAttributes(event.userPoolId, userName, { email_verified: 'true' });
-  }
+    // set email_verified attribute
+    if (status === 'EXTERNAL_PROVIDER') {
+      mlog('... external-provider: setting email_verified attribute ...');
+      await adminSetAttributes(event.userPoolId, userName, { email_verified: 'true' });
+    }
 
-  // send confirmation email
-  if (senderEmail) {
-    mlog('... sending confirmation email ...');
-    const { subject, message } = emailMaker
-      ? emailMaker(email, name, provider)
-      : defaultEmailMaker(email, name, provider);
-    await sendEmail(senderEmail, [email], subject, message);
-  }
+    // call updateDb function
+    if (updateDb) {
+      mlog('... calling updateDb ...');
+      await updateDb(user);
+    }
 
-  // response
-  mlog('... success ...');
-  return event; // must return event
-};
+    // send confirmation email
+    if (senderEmail) {
+      mlog('... sending confirmation email ...');
+      const { subject, message } = emailMaker
+        ? emailMaker(email, name, provider)
+        : defaultEmailMaker(email, name, provider);
+      await sendEmail(senderEmail, [email], subject, message);
+    }
+
+    // response
+    mlog('... success ...');
+    return event; // must return event
+  };
